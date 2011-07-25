67ca05548ab38eb44e7986c3b578852f939ed465
Daniel LaReaux <dlareaux@comcast.net>
2011-07-25 13:28:56 -0600

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,15 @@
-require 'spec_helper'
+require 'spec_helper.rb'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      response.should have_selector("title")
     end
   end
 
@@ -13,6 +17,19 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+     it "should have the right title" do
+      response.should have_selector("title")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+     it "should have the right title" do
+      response.should have_selector("title")
     end
   end
 
